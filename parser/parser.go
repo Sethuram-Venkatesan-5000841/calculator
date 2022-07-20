5796b323ee5a6ac5d63677c39903d90afe289ca5
Sethuram <sethuram@SUI-C0043-X0.local>
2022-07-20 11:54:08 +0530

    Update feature to parse

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,9 @@
 package parser
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 type Parser struct {
 	tokens []string
@@ -10,10 +13,21 @@ func NewParser() Parser {
 	return Parser{}
 }
 
-func (parser *Parser) Parse(input string) {
+func (parser *Parser) Parse(input string) (string, float64) {
 	if input == "" {
 		panic("cannot parse empty string")
 	}
 
 	parser.tokens = strings.Split(input, " ")
+
+	if len(parser.tokens) != 2 {
+		panic("invalid input")
+	}
+
+	operand, err := strconv.ParseFloat(parser.tokens[1], 64)
+	if err != nil {
+		panic("invalid operand")
+	}
+
+	return parser.tokens[0], operand
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,35 +11,51 @@ func TestNewParser(t *testing.T) {
 	})
 }
 
-func TestParser(t *testing.T) {
+func TestParser_Parse(t *testing.T) {
+	var parser Parser = NewParser()
 	t.Run("when parsing the string should not be empty", func(t *testing.T) {
 		assert.Panics(t, func() {
-			parser := NewParser()
 			parser.Parse("")
 		})
 	})
 
-	t.Run("should return ['add', '5'] for the input 'add 5'", func(t *testing.T) {
-		parser := NewParser()
-		parser.Parse("add 5")
-		assert.Equal(t, []string{"add", "5"}, parser.tokens)
+	t.Run("should panic when the input string does not have two tokens", func(t *testing.T) {
+		assert.Panics(t, func() {
+			parser.Parse("add")
+		})
+
+		assert.Panics(t, func() {
+			parser.Parse("add sub 5")
+		})
 	})
 
-	t.Run("should return ['subtract', '10'] for the input 'subtract 10'", func(t *testing.T) {
-		parser := NewParser()
-		parser.Parse("subtract 10")
-		assert.Equal(t, []string{"subtract", "10"}, parser.tokens)
+	t.Run("should return a string and a float value for the input 'divide 5.5'", func(t *testing.T) {
+		operator, operand := parser.Parse("divide 5.5")
+		assert.IsType(t, "", operator)
+		assert.IsType(t, 0.0, operand)
 	})
 
-	t.Run("should return ['multiply', '3'] for the input 'multiply 3'", func(t *testing.T) {
-		parser := NewParser()
-		parser.Parse("multiply 3")
-		assert.Equal(t, []string{"multiply", "3"}, parser.tokens)
+	t.Run("should return ['add', 5.0] for the input 'add 5'", func(t *testing.T) {
+		operator, operand := parser.Parse("add 5")
+		assert.Equal(t, "add", operator)
+		assert.Equal(t, 5.0, operand)
 	})
 
-	t.Run("should return ['divide', '2.5'] for the input 'divide 2.5'", func(t *testing.T) {
-		parser := NewParser()
-		parser.Parse("divide 2.5")
-		assert.Equal(t, []string{"divide", "2.5"}, parser.tokens)
+	t.Run("should return ['subtract', 10.0] for the input 'subtract 10'", func(t *testing.T) {
+		operator, operand := parser.Parse("subtract 10")
+		assert.Equal(t, "subtract", operator)
+		assert.Equal(t, 10.0, operand)
+	})
+
+	t.Run("should return ['multiply', 3.0] for the input 'multiply 3'", func(t *testing.T) {
+		operator, operand := parser.Parse("multiply 3")
+		assert.Equal(t, "multiply", operator)
+		assert.Equal(t, 3.0, operand)
+	})
+
+	t.Run("should return ['divide', 2.5] for the input 'divide 2.5'", func(t *testing.T) {
+		operator, operand := parser.Parse("divide 2.5")
+		assert.Equal(t, "divide", operator)
+		assert.Equal(t, 2.5, operand)
 	})
 }
