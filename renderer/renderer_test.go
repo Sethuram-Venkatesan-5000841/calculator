60e43011de3fcf3f3eb121d3eb810562c38d8fa8
Sethuram Venkatesan <sethuram.venkatesan@shopup.org>
2022-07-25 15:17:10 +0530

    Introduce renderer entity

--- renderer/renderer_test.go
+++ renderer/renderer_test.go
@@ -1,1 +1,58 @@
 package renderer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestViewSymbol(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+	ViewSymbol()
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	t.Run("should print > ", func(t *testing.T) {
+		assert.Equal(t, "> ", string(bytes))
+	})
+}
+
+func TestViewValue(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+	ViewValue(2.0)
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	t.Run("should print the value 2", func(t *testing.T) {
+		assert.Equal(t, "2\n", string(bytes))
+	})
+}
+
+func TestViewError(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+	ViewError("error")
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	t.Run("should print given error", func(t *testing.T) {
+		assert.Equal(t, "error\n", string(bytes))
+	})
+}
+
+func setupStdout() func() {
+	originalStdout := os.Stdout
+	return func() {
+		os.Stdout = originalStdout
+	}
+}
